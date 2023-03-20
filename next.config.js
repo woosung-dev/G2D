e8a17aa10acb74c9eb0cd6baf7eafbd604276a99
woosung <jangwooseng97@gmail.com>
2023-03-20 19:17:19 +0900

    feat(setting): sass사용을 위한 설정 추가 및 cors에러 처리

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable */ 
+// /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "/api/:path*",
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*',
       },
-    ];
+    ]
   },
 };
 
