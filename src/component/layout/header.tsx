d1ab14438c6c582ba393291ed222828f21be1d67
woosung <jangwooseng97@gmail.com>
2023-03-21 14:53:42 +0900

    feat(header:header 추가 화면 표시

---
+++ src/component/layout/header.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import style from './header.module.scss';
+
+export default function Header() {
+  return (
+    <Head>
+      <div className={style['header']}>
+        <div>
+          {/* <img src="/public/favicon.ico" alt="test-image" /> */}
+          <a href="/" className={style['logo']}>
+            Logo C2D
+          </a>
+        </div>
+        <div className={style['header-left']}>
+          <a href="/chat-text">Create</a>
+          <a href="upload-file">upload</a>
+        </div>
+      </div>
+    </Head>
+  );
+}
