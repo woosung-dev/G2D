88fb2b7f8a1c482ada4f4b912aa4b4397e364eae
woosung <jangwooseng97@gmail.com>
2023-04-06 23:07:49 +0900

    feat(layout-tailwind): header 및 main page 상단 배너 레이아웃 및 꾸미기

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -1,21 +1,44 @@
-import style from "./header.module.scss";
+import Link from "next/link";
 
 export default function Header() {
 	return (
 		<>
-			<div className={style["header"]}>
-				<div>
-					<a href="/" className={style["logo"]}>
-						<img src="/favicon.ico" alt="test-image" width={25} height={25} />{" "}
-						C2D
-					</a>
+			<header className="text-gray-600 body-font">
+				<div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+					<Link
+						href="/"
+						className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0"
+					>
+						<img src="/favicon.ico" alt="test-image" width={25} height={25} />
+						<span className="ml-3 text-xl">G2D</span>
+					</Link>
+					<nav className="flex flex-wrap items-center justify-center text-base cursor-pointer md:ml-auto">
+						<Link href={"/gtd"} className="mr-5 hover:text-gray-900 ">
+							New
+						</Link>
+						<Link href={"/chat-text"} className="mr-5 hover:text-gray-900">
+							Create
+						</Link>
+						<Link href={"/upload-file"} className="mr-5 hover:text-gray-900">
+							Upload
+						</Link>
+					</nav>
+					<button className="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0">
+						Button
+						<svg
+							fill="none"
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							className="w-4 h-4 ml-1"
+							viewBox="0 0 24 24"
+						>
+							<path d="M5 12h14M12 5l7 7-7 7"></path>
+						</svg>
+					</button>
 				</div>
-				<div className={style["header-left"]}>
-					<a href="/gtd">New</a>
-					<a href="/chat-text">Create</a>
-					<a href="upload-file">Upload</a>
-				</div>
-			</div>
+			</header>
 		</>
 	);
 }
