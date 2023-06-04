51c4347255acf7f9e007660a732f75946d0d49d6
woosung <jangwooseng97@gmail.com>
2023-06-05 02:32:12 +0900

    fix(fb): 피드백 처리3

--- src/component/modelViewer/index.tsx
+++ src/component/modelViewer/index.tsx
@@ -3,15 +3,20 @@ import Model from "react-3dmodelx";
 type Props = {
 	src: string;
 };
-const MViewer = ({ src }: Props) => (
-	<>
-		{src.slice(src.lastIndexOf(".")) === ".ply" && (
-			<Model.PLY src={src} backgroundColor="gray" />
-		)}
-		{src.slice(src.lastIndexOf(".")) === ".obj" && (
-			<Model.PLY src={src} backgroundColor="gray" />
-		)}
-	</>
-);
+const MViewer = ({ src }: Props) => {
+	console.log(src);
+	console.log(src.slice(src.lastIndexOf(".")));
+
+	return (
+		<>
+			{src.slice(src.lastIndexOf(".")) === ".ply" && (
+				<Model.PLY src={src} backgroundColor="gray" />
+			)}
+			{src.slice(src.lastIndexOf(".")) === ".obj" && (
+				<Model.OBJ src={src} backgroundColor="gray" />
+			)}
+		</>
+	);
+};
 
 export default MViewer;
